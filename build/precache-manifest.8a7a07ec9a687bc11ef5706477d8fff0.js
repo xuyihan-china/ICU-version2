c9dfa90c6d6c935d625ae3505f3fef988ac6ff89
xuyihan-china <848247256@qq.com>
2021-03-22 08:52:43 +0800

    homepage

---
+++ build/precache-manifest.8a7a07ec9a687bc11ef5706477d8fff0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af6a11de5e08065d8fdf40621725a0d",
+    "url": "/ICU-version2/index.html"
+  },
+  {
+    "revision": "26a3e8e0bf5177ec0c92",
+    "url": "/ICU-version2/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "cb2757dc099a37e9dc3d",
+    "url": "/ICU-version2/static/css/main.4f1d6332.chunk.css"
+  },
+  {
+    "revision": "26a3e8e0bf5177ec0c92",
+    "url": "/ICU-version2/static/js/2.08f56c4b.chunk.js"
+  },
+  {
+    "revision": "2f110bea0471aa7d6ce0050840ef4241",
+    "url": "/ICU-version2/static/js/2.08f56c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2757dc099a37e9dc3d",
+    "url": "/ICU-version2/static/js/main.6e71c396.chunk.js"
+  },
+  {
+    "revision": "ec4f0b54e9362a4022fb",
+    "url": "/ICU-version2/static/js/runtime-main.0d3dccbf.js"
+  },
+  {
+    "revision": "53544067365b10fb1b91fe405670caa1",
+    "url": "/ICU-version2/static/media/cty.53544067.png"
+  },
+  {
+    "revision": "bd8eedef243358eba7f3af8fe604bd02",
+    "url": "/ICU-version2/static/media/logo.bd8eedef.png"
+  }
+]);
